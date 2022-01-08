f2f34fe80c18ab6ed9581fb3d8f8a4198316c250
Luana <luana.mouro@gmail.com>
2022-01-08 13:20:40 -0300

    Operador

---
+++ Operador/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Operador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String usuario;
+            Console.WriteLine("Digite seu usuário:");
+            usuario = Console.ReadLine().ToUpper();
+
+            if (!(usuario =="ADMINISTRADOR"))
+            {
+                Console.WriteLine("Atenção, usuário sem poderes adminstrativos. Algumas tarefas serão bloqueadas.");
+            }
+        }
+    }
+}
